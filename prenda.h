0b67a9302b9d9fc8b97bd9ae22e8f083dc5feb9b
Alan-Rojas <117702235+Alan-Rojas@users.noreply.github.com>
2023-06-09 18:41:00 -0600

    Update prenda.h

--- prenda.h
+++ prenda.h
@@ -2,7 +2,7 @@
 Alan Rojas López 
 A01706146
 
-26/05/2023
+09/06/2023
 
 Declaración de clase Prendas
 
@@ -18,11 +18,14 @@ class Prendas {
     private: 
         string color;
         bool estampado;
-        int look;
+        int look; /*0: casual, 1: semi-formal, 2:formal*/
     
     public: 
         Prendas(): color(""), estampado(false), look(0){};
         Prendas(string col, bool est, int lo): color(col), estampado(est), look(lo){}; 
+
+        virtual void muestra_prenda() = 0;  // Declaramos la clase prenda cómo abstracta, ya que eso nos evita ambiguedades. 
+        //En este caso, que se generen objetos de Prenda, que no tengan un tipo espacífico: prendas superiores, prendas inferiores, y calzado. 
 
         string get_color(){
             return color;
@@ -47,10 +50,9 @@ class Prendas {
 
 class Prenda_Superior: public Prendas {
     private: 
-        string color, tipo;
-        bool estampado;
-        int look, mangas;
-
+        string color, tipo; /* Tipo de Prenda Superior: Polo, Camisa, Suéter, etc... */
+        bool estampado; 
+        int look, mangas; /*0: casual, 1: semi-formal, 2:formal*/ /*0: Sin Mangas, 1: Manga Corta, 2: Manga Larga*/
     public: 
         Prenda_Superior(): Prendas("", false, 0), tipo(""), mangas(0){};
         Prenda_Superior(string col, bool est, int lo, string tip, int man): color(col), estampado(est), look(lo), tipo(tip), mangas(man){};
@@ -68,18 +70,21 @@ class Prenda_Superior: public Prendas {
         void set_mangas(int man){
             mangas=man;
         }
+        void muestra_prenda(){
+            cout<<"Prenda Superior: \n""\tColor: "<<color<<"\tTipo: "<<tipo<<"\tEstampado:"<<estampado<<"\tLook: "<<look<<"\tMangas: "<<mangas<<"\n";
+        }
 };
 
 class Prenda_Inferior: public Prendas {
     private:
-        string color, material;
+        string color, material; /* Material: Mesclilla, Pants, etc...*/
         bool estampado;
-        int look, largo;
+        int look, largo; /*0: casual, 1: semi-formal, 2:formal*/   /* 0: Mini-shorts, 1: Shorts, 2: Pantalones*/
     
     public:
         Prenda_Inferior(): Prendas("", false, 0), material(""), largo(0){};
         Prenda_Inferior(string col, bool est, int lo, string mat, int lar): color(col), estampado(est), look(lo), material(mat), largo(lar){};
-
+ 
         string get_material(){
             return material;
         }
@@ -93,13 +98,16 @@ class Prenda_Inferior: public Prendas {
         void set_largo(int lar){
             largo=lar;
         }
+        void muestra_prenda(){
+            cout<<"Prenda Inferior: \n""\tColor: "<<color<<"\tMaterial: "<<material<<"\tEstampado: "<<estampado<<"\tLook: "<<look<<"\tLargo: "<<largo<<"\n";
+        }
 };
 
 class Calzado: public Prendas {
     private: 
         string color;
         bool estampado, calcetas;
-        int look;
+        int look; /*0: casual, 1: semi-formal, 2:formal*/ 
 
     public: 
         Calzado(): Prendas("", false, 0), calcetas(false){};
@@ -111,6 +119,9 @@ class Calzado: public Prendas {
 
         void set_calcetas(bool cal) {
             calcetas = cal;
+        }
+        void muestra_prenda(){
+            cout<<"Calzado: \n""\tColor: "<<color<<"Estampado: "<<estampado<<"\tCalcetas: "<<calcetas<<"\tLook: "<<look<<"\n";
         }
 };
 
